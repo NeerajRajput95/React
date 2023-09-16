c946fa18edd5e8281d9b79f7a4bfccbda67b9f2d
NeerajRajput95 <116864963+NeerajRajput95@users.noreply.github.com>
2023-09-17 00:16:30 +0530

    Expense Items

---
+++ src/Components/Expens.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Expens() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <h1>Food Rs 10</h1>
+      <h1>Petrol Rs 100</h1>
+      <h1>Movies Rs 200</h1>
+    </div>
+  );
+}
+
+export default Expens;
