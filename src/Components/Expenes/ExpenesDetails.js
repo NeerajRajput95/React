dffc6cf4173c02fe733df7ec89813f726e5bfc6f
NeerajRajput95 <116864963+NeerajRajput95@users.noreply.github.com>
2023-10-02 12:17:18 +0530

    change expense to 100$

--- src/Components/Expenes/ExpenesDetails.js
+++ src/Components/Expenes/ExpenesDetails.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./ExpenseDetails.css";
 import ExpenesDate from "./ExpenesDate";
 const ExpenesDetails = (props) => {
-  const [title, setTitle] = useState(props.Details.item);
+  const [title, setTitle] = useState(props.Details.price);
   // const handleClick = () => {
   //   props.Details.handleClick(title); // Pass the itemName to handleClick
   // };
   const handlechange = () => {
-    setTitle("updated");
+    setTitle("100$");
   };
   console.log(props, "ddddddd");
   return (
@@ -15,10 +15,10 @@ const ExpenesDetails = (props) => {
       <div className="ExpenesItem">
         <ExpenesDate currentDate={props.Details} />
         {console.log("xxxxxxx", title)}
-        <div className="ExpenseItems">{title}</div>
+        <div className="ExpenseItems">{props.Details.item}</div>
         <div className="item">
           <h3 className="">{props.Details.location}</h3>
-          <div className="price">Rs {props.Details.price}</div>
+          <div className="price">{title}</div>
           {/* <button onClick={handleClick}>Delete</button> */}
           <button onClick={handlechange}>change title</button>
         </div>
